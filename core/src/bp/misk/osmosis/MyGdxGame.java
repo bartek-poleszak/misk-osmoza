132d7466611cbfc286808187aa844731c4701a68
Bartek Poleszak <bp@bp-samsung>
2016-05-27 12:33:14 +0200

    zaorane

--- core/src/bp/misk/osmosis/MyGdxGame.java
+++ core/src/bp/misk/osmosis/MyGdxGame.java
@@ -14,9 +14,7 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class MyGdxGame extends ApplicationAdapter {
 	private Stage stage;
     private ShapeRenderer shapeRenderer;
-    private CollisionManager collisionManager = new CollisionManager();
     public static Texture sampleTexture;
-    private ConcentrationGrid concentrationGrid = new ConcentrationGrid();
     private FPSLogger fpsLogger = new FPSLogger();
 	
 	@Override
@@ -25,64 +23,10 @@ public class MyGdxGame extends ApplicationAdapter {
         sampleTexture = new Texture(Gdx.files.internal("badlogic.jpg"));
 		stage = new Stage(new ScreenViewport());
         shapeRenderer = new ShapeRenderer();
-        ParticleGroupBuilder builder = new ParticleGroupBuilder(shapeRenderer);
-        builder.setRadius(5);
-        ParticleGroup water = builder.create();
-        for (Actor actor : water.getChildren())
-            collisionManager.addParticles((DotActor) actor);
-
-        ParticleGroupBuilder dustBuilder = new ParticleGroupBuilder(shapeRenderer);
-        dustBuilder.setAmount(10);
-        dustBuilder.setRadius(10);
-        dustBuilder.setBorders(20, 20, 200, 200);
-        ParticleGroup salt = dustBuilder.create();
-        salt.setDotColor(Color.ORANGE);
-        for (Actor actor : salt.getChildren())
-            collisionManager.addParticles((DotActor) actor);
-
-        concentrationGrid.setParticles(collisionManager.getParticles());
-        concentrationGrid.setCellDensity(2, 2);
-        stage.addActor(water);
-        stage.addActor(salt);
-
-        createBorders();
-
 		Gdx.input.setInputProcessor(stage);
 	}
 
     private void createBorders() {
-        VerticalBorderActor leftBorder = new VerticalBorderActor(shapeRenderer);
-        int bottom = 10;
-        int left = 10;
-        int width = 800;
-        int height = 750;
-        leftBorder.setLinePosition(bottom, left, height);
-        VerticalBorderActor rightBorder = new VerticalBorderActor(shapeRenderer);
-        rightBorder.setLinePosition(width, left, height);
-        HorizontalBorderActor bottomBorder = new HorizontalBorderActor(shapeRenderer);
-        bottomBorder.setLinePosition(bottom, left, width);
-
-        HorizontalBorderActor topLeft = new HorizontalBorderActor(shapeRenderer);
-        topLeft.setLinePosition(bottom, height - 300, width / 2);
-        HorizontalBorderActor topRight = new HorizontalBorderActor(shapeRenderer);
-        topRight.setLinePosition(bottom + width / 2, height - 400, width / 2);
-
-        collisionManager.addBorder(leftBorder);
-        collisionManager.addBorder(rightBorder);
-        collisionManager.addBorder(bottomBorder);
-        collisionManager.addBorder(topLeft);
-        collisionManager.addBorder(topRight);
-        stage.addActor(leftBorder);
-        stage.addActor(rightBorder);
-        stage.addActor(bottomBorder);
-        stage.addActor(topLeft);
-        stage.addActor(topRight);
-
-        Membrane membrane = new Membrane(width / 2, bottom, height, 15, shapeRenderer);
-        collisionManager.setMembrane(membrane);
-        stage.addActor(membrane);
-
-        concentrationGrid.setBorders(left, bottom, left + width, bottom + height);
     }
 
 
@@ -97,12 +41,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        collisionManager.act();
         stage.act();
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//        concentrationGrid.checkConcentration();
-//        concentrationGrid.draw(shapeRenderer);
         stage.draw();
         shapeRenderer.end();
         fpsLogger.log();
